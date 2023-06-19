2ea4bd2b89c3c7f50aaf18c883e673e5469152a7
John Santosuosso <john.santosuosso1@gmail.com>
2023-06-19 14:31:07 -0400

    add delete and find functionality

--- bst_files/bst.rb
+++ bst_files/bst.rb
@@ -51,4 +51,78 @@ class Tree
       end
     end
   end
+
+  def find(value)
+    node = @root
+    loop do
+      return node if node.data == value
+      return nil if node.left.nil? && node.right.nil?
+
+      node = value < node.data ? node.left : node.right
+    end
+  end
+
+  def delete(value)
+    node = find(value)
+    return nil if node.nil?
+    require 'pry'; binding.pry 
+
+    node_parent = node_parent(value)
+
+    #Check to see if the node we're deleting has children
+    if node.left.nil? && node.right.nil?
+      return @root.data = nil if node_parent.nil?
+
+      node_parent.left == node ? node_parent.left = nil : node_parent.right = nil
+      return
+    end
+    #Check to see if node we're deleting has a right child
+    if node.left.nil? && !node.right.nil?
+      node_parent.left = (node_parent.left.data == data ? node.right : node.right)
+      return
+    end
+
+    #Check to see if node we're deleting has a left child
+    if node.right.nil? && !node.left.nil?
+      node_parent.left = (node_parent.left.data == data ? node.right : node.right)
+      return
+    end
+
+    #If none of the above and the node has two children
+    node_min = node_min_in_r(node)
+    delete(node_min.data)
+    node.data = node_min.data
+  end
+
+  def node_parent(value)
+    return nil if @root.data == value
+
+    node = @root
+    loop do
+      if value < node.data
+        return node if node.left.data == value
+
+        node = node.left
+      else
+        return node if node.right.data == value
+      end
+
+      return nil if node.left.nil? && node.right.nil?
+    end
+  end
+
+  def node_min_in_r(node)
+    node = node.right
+    until node.left == node.right
+      if node.left.nil?
+        node = node.right
+        next
+      end
+      node = node.left
+    end
+    node
+  end
+
+  private :node_min_in_r
+
 end
--- bst_files/bst_spec.rb
+++ bst_files/bst_spec.rb
@@ -4,15 +4,23 @@ RSpec.describe Tree do
   let(:arr) { [2,1,3,5,4] }
   let(:tree) { Tree.new(arr) }
   
-  it "instantiates an object" do
+  it 'instantiates an object' do
     expect(tree.root).to be_a(Node)
     expect(tree.root.left.data).to eql 2
   end
 
-  it "inserts a value" do
+  it 'inserts a value' do
     tree.insert(6)
-    require 'pry'; binding.pry 
     expect(tree.root.right.right.data).to eql 6
     expect(tree.root.right.data).to eql 5
+  end
+
+  it 'finds a value' do
+    result = tree.find(5)
+    expect(result.data).to eql 5
+  end
+
+  it 'deletes a value' do
+    require 'pry'; binding.pry 
   end
 end
